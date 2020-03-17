d9b9aae3d44f1c87650840a3b17f55188d80786d
MSeidelFed <54093698+MSeidelFed@users.noreply.github.com>
2020-03-17 22:46:49 +1100

    Update plotting_distributions.R

--- plotting_distributions.R
+++ plotting_distributions.R
@@ -2,7 +2,8 @@ plotting_distributions <- function(Distribution_test_mat = test,
                                    Regression_Family = gamma,
                                    n_random_distributions = 100,
                                    colour = "red",
-                                   random_distributions_plot = F) {
+                                   random_distributions_plot = F,
+                                   vector_colors = c(dim(Distribution_test_mat)[1])) {
   
   if(!require(fitdistrplus)){install.packages("fitdistrplus"); require(fitdistrplus)}
   library(fitdistrplus)
@@ -32,59 +33,53 @@ for (i in 1:dim(Distribution_test_mat)[2]) {
 average_distribution_plot <- t(rbind("square of skewness" = list2df(SKEW)^2, kurtosis = list2df(KURT)))
 
 ## choosing a distribution
-
 if (Regression_Family == "gamma") {
 
 ### vectors of gamma distribution to estimate intercept and slope
+test <- matrix(data = NA, nrow = dim(Distribution_test_mat)[1], ncol = n_random_distributions)
 
-      test <- matrix(data = NA, nrow = dim(Distribution_test_mat)[1], ncol = n_random_distributions)
-
-      for (i in 1:n_random_distributions) {
+         for (i in 1:n_random_distributions) {
   
-      x <- round(rgamma(n = dim(Distribution_test_mat)[1],shape = 2,rate = 0.2),1)
+         x <- round(rgamma(n = dim(Distribution_test_mat)[1],shape = 2,rate = 0.2),1)
   
-      test[,i] <- x
+                        test[,i] <- x
   
 }} else if (Regression_Family == "logis") {
   
 ### vectors of logis distribution to estimate intercept and slope
+test <- matrix(data = NA, nrow = dim(Distribution_test_mat)[1], ncol = n_random_distributions)
   
-                test <- matrix(data = NA, nrow = dim(Distribution_test_mat)[1], ncol = n_random_distributions)
-  
-                for (i in 1:n_random_distributions) {
+          for (i in 1:n_random_distributions) {
     
-                x <- round(rlogis(n = dim(Distribution_test_mat)[1],location = 2,scale = 0.2),1)
+          x <- round(rlogis(n = dim(Distribution_test_mat)[1],location = 2,scale = 0.2),1)
     
-                test[,i] <- x
+                        test[,i] <- x
   
 }} else if (Regression_Family == "Beta") {
                   
 ### vectors of beta distribution to estimate intercept and slope
+test <- matrix(data = NA, nrow = dim(Distribution_test_mat)[1], ncol = n_random_distributions)
                   
-                        test <- matrix(data = NA, nrow = dim(Distribution_test_mat)[1], ncol = n_random_distributions)
-                  
-                        for (i in 1:n_random_distributions) {
+          for (i in 1:n_random_distributions) {
                     
-                        x <- round(rbeta(n = dim(Distribution_test_mat)[1],shape1 = 2,shape2 = 1, ncp = 5),1)
+          x <- round(rbeta(n = dim(Distribution_test_mat)[1],shape1 = 2,shape2 = 1, ncp = 5),1)
                     
                         test[,i] <- x
                     
 }} else if (Regression_Family == "Normal") {
                           
 ### vectors of normal distribution to estimate intercept and slope
+test <- matrix(data = NA, nrow = dim(Distribution_test_mat)[1], ncol = n_random_distributions)
                           
-                              test <- matrix(data = NA, nrow = dim(Distribution_test_mat)[1], ncol = n_random_distributions)
-                          
-                              for (i in 1:n_random_distributions) {
+          for (i in 1:n_random_distributions) {
                             
-                              x <- round(rnorm(n = dim(Distribution_test_mat)[1],mean = 0, sd = 1),1)
+          x <- round(rnorm(n = dim(Distribution_test_mat)[1],mean = 0, sd = 1),1)
                             
-                              test[,i] <- x
+                        test[,i] <- x
                             
 }}else { NA }
 
 ## estimating kurtosis and square of skewness of the random distribution vectors
-
 SKEW <- vector(mode = "list", length = dim(test)[2])
 KURT <- vector(mode = "list", length = dim(test)[2])
 
@@ -96,29 +91,22 @@ test_distribution_plot <- t(rbind("square of skewness" = list2df(SKEW)^2, kurtos
 
 
 ## fitting a linear model to the two parameters in order to get the slope and intercept for the graph
-
 model<- lm(formula = test_distribution_plot[,2]~ test_distribution_plot[,1])
 
 model_intercept <- model$coefficients[1]
 
 model_slope <- model$coefficients[2]
 
-
 ## plot of your variables´ distributions plus a line featuring a selected distribution
-
-plot(average_distribution_plot, ylim = c(10,0), xlim = c(0,5))
+plot(average_distribution_plot, ylim = c(10,0), xlim = c(0,5), col = vector_colors)
 
 abline(a = model_slope, b = model_intercept, col = colour)
 
-if (random_distributions_plot == F) {
-
-NA
-  
-} else { 
+if (random_distributions_plot == F) { NA } else { 
   
   plot(test_distribution_plot, ylim = c(10,0), xlim = c(0,5))
   
-}
+                                                } 
 
 return(average_distribution_plot)
 
